8696dd10b65a51dd949c66062313439b4d0be0ab
valjacques Nyemb <nyvanyemb@gmail.com>
2024-03-22 02:03:39 +0100

    fix roman_to_int file problem again

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -13,10 +13,12 @@ def roman_to_int(roman_string):
         'M': 1000
     }
     total = 0
+    i = 0
     for i in range(len(roman_string) - 1):
         if roman[roman_string[i]] > roman[roman_string[i + 1]]:
             total += roman[roman_string[i]]
         else:
             total -= roman[roman_string[i]]
+        i += 1
     total += roman[roman_string[i]]
     return total
